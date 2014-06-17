44b3cbc048dc5804dfbaab3f7c40798efa097de1
cush <ben.cushing@agilex.com>
2014-06-17 01:57:45 -0400

    Created prototype UI

---
+++ client/proto/src/data/StripData.js
@@ -0,0 +1,8 @@
+define(function(require, exports, module) {
+    module.exports = [
+        {title: 'New', iconUrl: 'img/strip-icons/famous.png'},
+        {title: 'Tracked', iconUrl: 'img/strip-icons/friends.png'},
+        {title: 'Mine', iconUrl: 'img/strip-icons/starred.png'},
+        {title: 'Past', iconUrl: 'img/strip-icons/settings.png'}
+    ];
+});
---
+++ client/proto/src/views/FeaturedView.js
@@ -0,0 +1,71 @@
+define(function(require, exports, module) {
+    var View            = require('famous/core/View');
+    var Surface         = require('famous/core/Surface');
+    var Transform       = require('famous/core/Transform');
+    var StateModifier   = require('famous/modifiers/StateModifier');
+    var ImageSurface    = require('famous/surfaces/ImageSurface');
+
+    function FeaturedView() {
+        View.apply(this, arguments);
+
+        createBacking.call(this);
+        createNoteworthy.call(this);
+        createStaffPicks.call(this);
+    }
+
+    FeaturedView.DEFAULT_OPTIONS = {
+        angle: 0,
+        xOffset: 15,
+        fontSize: 22
+    };
+
+    var createBacking = function() {
+        var backSurface = new Surface({
+            size: [undefined, 60],
+            properties : {
+                backgroundColor : '#e9e9e9'
+            }
+        });
+        var modifier = new StateModifier({
+            transform: Transform.thenMove(Transform.rotateZ(this.options.angle), [-55, 80, 0])
+        });
+        this.add(modifier).add(backSurface);
+    };
+
+    var createNoteworthy = function() {
+        var surface = new Surface({
+            size: [true, true],
+            content: 'Town Hall @',
+            properties: {
+                color: 'black',
+                fontSize: this.options.fontSize + 'px',
+                textTransform: 'uppercase',
+                pointerEvents : 'none'
+            }
+        });
+
+        var modifier = new StateModifier({
+            transform: Transform.thenMove(Transform.rotateZ(this.options.angle), [this.options.xOffset, 30, 0])
+        });
+
+        this.add(modifier).add(surface);
+    };
+
+    var createStaffPicks = function() {
+        var surface = new ImageSurface({
+            size: [200, true],
+            content: 'img/Agilex_logo.png'
+        });
+
+        var modifier = new StateModifier({
+            transform: Transform.thenMove(Transform.rotateZ(this.options.angle), [this.options.xOffset, 67, 0])
+        });
+
+        this.add(modifier).add(surface);
+    };
+
+    FeaturedView.prototype = Object.create(View.prototype);
+    FeaturedView.prototype.constructor = FeaturedView;
+
+    module.exports = FeaturedView;
+});
