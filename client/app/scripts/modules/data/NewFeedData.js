f352b0b74ed9b35cf26932206039b1cadb61d984
cush <ben.cushing@agilex.com>
2014-06-28 16:18:56 -0400

    Added id to bootstrap data

--- client/app/scripts/modules/data/NewFeedData.js
+++ client/app/scripts/modules/data/NewFeedData.js
@@ -1,16 +1,16 @@
 define(function(require, exports, module) {
     module.exports = [
-        {title: 'Wouldn\'t it be fun to watch soccer on Friday afternoons?', comments: '1', tracked: 'true', voted: 'false', locked: 'false'},
-        {title: 'We should have hackathons on Friday afternoons w/ Node as a theme.', comments: '0', tracked: 'true', voted: 'false', locked: 'false'},
-        {title: 'How about we have dippin\' dots on Friday afternoons?', comments: '1', tracked: 'true', voted: 'false', locked: 'false'},
-        {title: 'I believe all UI/UX designers should get a raise.', comments: '2', tracked: 'true', voted: 'false', locked: 'false'},
-        {title: 'Foosball should be a required activity during the day', comments: '3', tracked: 'true', voted: 'false', locked: 'false'},
-        {title: 'I believe all NODE developers should get a raise', comments: '0', tracked: 'true', voted: 'false', locked: 'false'},
-        {title: 'I think we should use NODE for every and all projects from now on at Agilex', comments: '0', tracked: 'true', voted: 'false', locked: 'false'},
-        {title: 'How about we get together for carrot cake on Thursday mornings?', comments: '1', tracked: 'true', voted: 'false', locked: 'false'},
-        {title: 'I believe all scrum masters should get a raise', comments: '2', tracked: 'true', voted: 'false', locked: 'false'},
-        {title: 'I think we should use Target Process for every and all projects from now on at Agilex', comments: '1', tracked: 'true', voted: 'false', locked: 'false'},
-        {title: 'How about we get together for soccer on Wednesday nights?', comments: '0', tracked: 'true', voted: 'false', locked: 'false'},
-        {title: 'How many people would like to have ALOHA FRIDAYS?! You wear Hawaiian shirts to work :).', comments: '4', tracked: 'true', voted: 'false', locked: 'false'}
+        {id: '1', title: 'Wouldn\'t it be fun to watch soccer on Friday afternoons?', comments: '1', tracked: 'true', voted: 'false', locked: 'false'},
+        {id: '2', title: 'We should have hackathons on Friday afternoons w/ Node as a theme.', comments: '0', tracked: 'true', voted: 'false', locked: 'false'},
+        {id: '3', title: 'How about we have dippin\' dots on Friday afternoons?', comments: '1', tracked: 'true', voted: 'false', locked: 'false'},
+        {id: '4', title: 'I believe all UI/UX designers should get a raise.', comments: '2', tracked: 'true', voted: 'false', locked: 'false'},
+        {id: '5', title: 'Foosball should be a required activity during the day', comments: '3', tracked: 'true', voted: 'false', locked: 'false'},
+        {id: '6', title: 'I believe all NODE developers should get a raise', comments: '0', tracked: 'true', voted: 'false', locked: 'false'},
+        {id: '7', title: 'I think we should use NODE for every and all projects from now on at Agilex', comments: '0', tracked: 'true', voted: 'false', locked: 'false'},
+        {id: '8', title: 'How about we get together for carrot cake on Thursday mornings?', comments: '1', tracked: 'true', voted: 'false', locked: 'false'},
+        {id: '9', title: 'I believe all scrum masters should get a raise', comments: '2', tracked: 'true', voted: 'false', locked: 'false'},
+        {id: '10', title: 'I think we should use Target Process for every and all projects from now on at Agilex', comments: '1', tracked: 'true', voted: 'false', locked: 'false'},
+        {id: '11', title: 'How about we get together for soccer on Wednesday nights?', comments: '0', tracked: 'true', voted: 'false', locked: 'false'},
+        {id: '12', title: 'How many people would like to have ALOHA FRIDAYS?! You wear Hawaiian shirts to work :).', comments: '4', tracked: 'true', voted: 'false', locked: 'false'}
     ];
 });
