5e8e2fdba44464c1ab86b1de127f24b8ce2c0517
Sean Dillon <seanmdillon@gmail.com>
2014-07-08 13:59:26 -0400

    Updated fetchIdeas calls.  Tracked, Mine, Past, etc.

--- services/apexclient.js
+++ services/apexclient.js
@@ -173,27 +173,29 @@ exports.trackedIdeasFeed = trackedIdeasFeed = function(user, successHandler, err
 exports.myItemsFeed = myItemsFeed = function(user, successHandler, errorHandler) {
 	logger.info('apexClient.myItemsFeed(%s)', user);
 	var lowerRowCount = 1;
-	var upperRowCount = 10;
+	var upperRowCount = 25;
 	var sql =
-		"select *  " +
+		"select * " +
 		"  from ( select rownum rnum, a.* " +
-		"           from ( select x.id, x.short_desc, x.description, x.created, " +
-		"                         x.comment_count, x.tags, x.rn " +
-		"                    from (select i.id, i.short_desc, i.description, i.created, " +
-		"                                 c.comment_count, rownum rn, t.tags   " +
-		"                            from ideas i, " +
+		"           from ( select x.id, x.owner, x.topic_id, x.topic_title, x.short_desc, x.description, " +
+		"                         x.created, x.comment_count, x.tags, x.rn " +
+		"                    from (select i.id, i.owner, t.id topic_id, t.title topic_title, i.short_desc, " +
+		"                                 i.description, i.created, c.comment_count, rownum rn, t.tags " +
+		"                            from ideas i, topics t, " +
 		"                                 (select count(*) comment_count, parent_id " +
 		"                                    from comments " +
 		"                                   where parent_type = 'idea' " +
 		"                                   group by parent_id) c, " +
-		"                                 (select it.idea_id,  " +
+		"                                 (select it.idea_id, " +
 		"                                         listagg(it.tag, ' ') within group (order by it.tag) tags " +
 		"                                    from idea_tags it " +
 		"                                   group by it.idea_id) t " +
-		"                           where c.parent_id(+) = i.id " +
-		"                             and i.id = t.idea_id(+) ) x ) a  " +
+		"                           where i.owner = '" + user + "' " +
+		"                             and i.topic_id = t.id(+) " +
+		"                             and c.parent_id(+) = i.id " +
+		"                             and i.id = t.idea_id(+) ) x ) a " +
 		"          where rownum <= " + upperRowCount + " ) " +
-		" where rnum >= " + lowerRowCount;
+		" where rnum >= " + lowerRowCount; 
 
 	ideaFeed(sql, [], successHandler, errorHandler);
 };
@@ -202,6 +204,30 @@ exports.pastItemsFeed = pastItemsFeed = function(user, successHandler, errorHand
 	var lowerRowCount = 1;
 	var upperRowCount = 10;
 	var sql =
+		"select * " +
+		"  from ( select rownum rnum, a.* " +
+		"           from ( select x.id, x.owner, x.topic_id, x.topic_title, x.short_desc, x.description, " +
+		"                         x.created, x.comment_count, x.tags, x.rn " +
+		"                    from (select i.id, i.owner, t.id topic_id, t.title topic_title, i.short_desc, " +
+		"                                 i.description, i.created, i.closed, c.comment_count, ta.tags, rownum rn " +
+		"                            from ideas i, topics t, " +
+		"                                 (select count(*) comment_count, parent_id " +
+		"                                    from comments " +
+		"                                   where parent_type = 'idea' " +
+		"                                   group by parent_id) c, " +
+		"                                 (select it.idea_id, " +
+		"                                         listagg(it.tag, ' ') within group (order by it.tag) tags " +
+		"                                    from idea_tags it " +
+		"                                   group by it.idea_id) ta " +
+		"                           where t.id (+) = i.topic_id " +
+		"                             and i.id = c.parent_id (+) " +
+		"                             and i.id = ta.idea_id (+) " +
+		"                             and (i.closed is not null or " +
+		"                                  i.id in (select idea_id from votes where voter = '" + user + "'))) x ) a " +
+		"          where rownum <= " + upperRowCount + " ) " +
+		" where rnum >= " + lowerRowCount;
+
+/*
 		"select *  " +
 		"  from ( select rownum rnum, a.* " +
 		"           from ( select x.id, x.short_desc, x.description, x.created, " +
@@ -221,6 +247,7 @@ exports.pastItemsFeed = pastItemsFeed = function(user, successHandler, errorHand
 		"                             and i.id = t.idea_id(+) ) x ) a  " +
 		"          where rownum <= " + upperRowCount + " ) " +
 		" where rnum >= " + lowerRowCount;
+*/
 
 	ideaFeed(sql, [], successHandler, errorHandler);
 };
